9137be407d903dea8d0021984e04b27bf0747e99
kittipat19877 <number8671@gmail.com>
2023-06-27 15:27:48 +0700

    create profile-card

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+function App() {
+  return (
+    <>
+      <div className='card'>
+        <Avatar />
+        <div className='data'>
+          <Intro />
+          <Skilllist />
+        </div>
+      </div>
+    </>
+  )
+}
+function Skilllist() {
+  return(
+    <div className='skill-list'>
+      <Skill skill="Unity" emoji="💀" color="grey" />
+      <Skill skill="PHP" emoji="🔥" color="grey" />
+      <Skill skill="REACT" emoji="💀" color="grey" />
+      <Skill skill="MySQL" emoji="🔥" color="grey" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className='skill' style={{ backgroundColo: props.color }}> 
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+    <h1>Porsche</h1>
+    <p>
+      มาเฟียชอบสตรีทฟู้ดอินเดีย 🏴‍☠️
+    </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className='avata' src="pc.jpg" alt='My Avatar'/>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +55,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
